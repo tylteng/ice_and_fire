a2ead481e09eee263724f0f83961e62e48825855
tylteng <teng.ty4@gmail.com>
2017-12-11 14:32:00 -0500

    completed exercise

--- test/factories/riders.rb
+++ test/factories/riders.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :rider do
+    name "Aegon"
+    nickname "The Conqueror"
+    email {"#{name}@westeros.com"}
   end
 end
--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,8 +19,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
-
+    young_dragon = build(:dragon, age: 2)
     # not allowed to change the assert statement!
     skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
@@ -28,7 +27,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 50)
 
     # not allowed to change the assert statement!
     skip unless middle_dragon
@@ -37,7 +36,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 1000)
 
     # not allowed to change the assert statement!
     skip unless ancient_dragon
@@ -46,8 +45,8 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_stranger_cannot_mount
     # use your knowledge of factories to make this test pass!
-    rider = nil
-    dragon = nil
+    rider = build(:rider)
+    dragon = build(:dragon, name:"Balerion")
 
     # not allowed to change the assert statement!
     skip unless rider && dragon
@@ -58,8 +57,8 @@ class DragonTest < ActiveSupport::TestCase
     # use your knowledge of factories to make this test pass!
     # Note: you'll need to do more than just supply the right names...
     # Hint: you can pass variables as properties too, not just strings!
-    rider = nil
-    dragon = nil
+    rider = create(:rider)
+    dragon = build(:dragon, name:"Balerion", rider_id: rider.id)
 
     # not allowed to change the assert statement!
     skip unless rider && dragon
